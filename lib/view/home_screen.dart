49d3a65cefa083ed2ad45c800b900689582a4b70
reiji.tomooka <tomooka.04172@gmail.com>
2023-07-30 16:51:52 +0900

    Version情報の表示を追加

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:go_router/go_router.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  Future<String> getVersionInfo() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    var text = 'App Ver. ${packageInfo.version}';
+    return text;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +18,7 @@ class HomeScreen extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Padding(padding: EdgeInsets.only(bottom: 400)),
           const Text('Guess The City')
               .animate(
                 onPlay: (controller) => controller.repeat(
@@ -24,6 +32,28 @@ class HomeScreen extends StatelessWidget {
               GoRouter.of(context).go('/game');
             },
             child: const Text('Go Game'),
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 400)),
+          FutureBuilder(
+            future: getVersionInfo(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      snapshot.hasData ? snapshot.data : '',
+                      style:
+                          Theme.of(context).textTheme.headlineSmall?.copyWith(
+                                fontSize: 14.0,
+                              ),
+                      textAlign: TextAlign.right,
+                    ),
+                  ),
+                ],
+              );
+            },
           ),
         ],
       ),
